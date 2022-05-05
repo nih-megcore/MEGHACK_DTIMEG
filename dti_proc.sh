c0f456aa397f572cd6ae914761548ff1504a29ec
Jeff Stout <jstout211@yahoo.com>
2022-05-05 15:51:52 -0400

    Update dti_proc.sh

--- dti_proc.sh
+++ dti_proc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-subjid=sub-ON02747 #$1
+subjid=$1
 
 #Setup Input and Output Directories
 output_dir=/gpfs/gsfs12/users/NIMH_scratch/MEG_Hackathon/DTI_MEG
@@ -13,9 +13,9 @@ T2path=${bids_dir}/${subjid}/ses-01/anat/${subjid}_ses-01_*run-01_T2w.nii.gz
 DWI_r1=${bids_dir}/${subjid}/ses-01/dwi/${subjid}_ses-01_run-01_dwi.nii.gz
 DWI_r2=${bids_dir}/${subjid}/ses-01/dwi/${subjid}_ses-01_run-02_dwi.nii.gz
 
-#Copy T1 and T2 into reg folder
-regpath=/tmp/regT1T2
-mkdir ${regpath}
-3dcopy ${T1path} ${regpath}
-3dcopy ${T2path} ${regpath}
 
+preproc_dir=/gpfs/gsfs12/users/NIMH_scratch/MEG_Hackathon/DTI_MEG/preproc
+
+cpdir=${preproc_dir}/${subjid}
+mkdir ${cpdir}
+for i in ${T1path} ${T2path} ${DWI_r1} ${DWI_r2}; do cp $i ${cpdir}; done
