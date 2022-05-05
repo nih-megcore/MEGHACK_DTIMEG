6ce3cfad13438b129b20a1521a8d64f0f0d2f2f7
Jeff Stout <jstout211@yahoo.com>
2022-05-05 15:30:52 -0400

    Initial

---
+++ dti_proc.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+subjid=sub-ON02747 #$1
+
+#Setup Input and Output Directories
+output_dir=/gpfs/gsfs12/users/NIMH_scratch/MEG_Hackathon/DTI_MEG
+bids_dir=/gpfs/gsfs12/users/NIMH_scratch/MEG_Hackathon/hv_bids
+
+#Define MRI Paths
+T1path=${bids_dir}/${subjid}/ses-01/anat/${subjid}_ses-01_*run-01_T1w.nii.gz
+T2path=${bids_dir}/${subjid}/ses-01/anat/${subjid}_ses-01_*run-01_T2w.nii.gz
+
+DWI_r1=${bids_dir}/${subjid}/ses-01/dwi/${subjid}_ses-01_run-01_dwi.nii.gz
+DWI_r2=${bids_dir}/${subjid}/ses-01/dwi/${subjid}_ses-01_run-02_dwi.nii.gz
+
+#Copy T1 and T2 into reg folder
+#3dcopy 
