0cde538cd7b248e11a8a1d6d83526ab834470899
Price Withers <price.withers@nih.gov>
2022-05-06 11:22:06 -0400

    FIX: update dti_to_standard_space.sh

--- dti_to_standard_space.sh
+++ dti_to_standard_space.sh
@@ -1,25 +1,37 @@
 #!/bin/bash
 
-subjid="ON95742"
+subjid=$1
 
 # setup directories and paths
+odir=/gpfs/gsfs12/users/NIMH_scratch/MEG_Hackathon/DTI_MEG/dti
 bids_dir=/gpfs/gsfs12/users/NIMH_scratch/MEG_Hackathon/hv_bids
 anat_dir=${bids_dir}/${subjid}/ses-01/anat
 dwi_dir=${bids_dir}/${subjid}/ses-01/dwi
 
-T1path=${anat_dir}/${subjid}_ses-01_*run-01_T1w.nii.gz
-FApath=${dwi_dir}/dti_FA.nii.gz
+T1file=${subjid}_ses-01_*run-01_T1w.nii.gz
+FAfile=dti_FA.nii.gz
+
+cd ${anat_dir} || exit
 
 # align T1 to TT_N27
 @auto_tlrc \
     -base TT_N27+tlrc               \
-    -input $T1path                  \
-    -prefix ${anat_dir}/t1+tlrc.nii \
+    -input $T1file                  \
+    -prefix t1+tlrc.nii \
     -no_avoid_eyes
+
+# remove temporary files
+rm *+orig*
+rm *.1D
+rm *WarpDrive.log
 
 # align FA map to t1+tlrc
 
+cd ${dwi_dir} || exit
+
 @auto_tlrc \
-    -apar ${anat_dir}/t1+tlrc.nii   \
-    -input ${FApath}                \
-    -prefix ${dwi_dir}/FA+tlrc.nii
+    -apar ../anat/t1+tlrc.nii   \
+    -input $FAfile                \
+    -prefix ${subjid}_FA_tlrc.nii
+
+cp ${subjid}_FA_tlrc.nii ${odir}/${subjid}_FA_tlrc.nii
