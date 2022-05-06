43c6aaf25e35e43ca6c41819e92b03838aa8d0bc
Price Withers <price.withers@nih.gov>
2022-05-06 13:45:44 -0400

    Add function to average regions and return 1D LFP vector

--- helpers.py
+++ helpers.py
@@ -4,12 +4,67 @@ import numpy as np
 import nibabel as nb
 import mne
 
-def read_chnames(fpath):
+def read_fif(fpath):
+    """Return mne.evoked.Evoked structure given a fpath.
+
+    Args:
+        fpath (str): path to .fif file
+
+    Returns:
+        mne.evoked.Evoked: _description_
+    """
+
     ave=mne.read_evokeds(fpath)
     ave=ave[0]
     ave.pick_types(meg=True, ref_meg=False)
-    ch_regions=set([i[1:3] for i in ave.info["ch_names"]])
-    return ch_regions
+    return ave
+
+def get_regions(ave):
+    """Retrieve list of regions for specifying vector order.
+
+    Args:
+        ave (mne.evoked.Evoked): _description_
+
+    Returns:
+        list: list of regions
+    """
+
+    return list(set([i[1:3] for i in ave.info["ch_names"]]))
+
+def compute_lfp_vector(order, ave):
+    """Return ordered 1D vector of LFPs.
+
+    Args:
+        order (list): order of channel regions
+        ave (mne.evoked.Evoked): _description_
+
+    Returns:
+        np.array: 1D vector of LFPs in order
+    """
+
+    ave_data = ave._data
+    region_dict = {}
+
+    # divide ave_data into regions
+    for i, ch_name in enumerate(ave.info.ch_names):
+        region = ch_name[1:3]
+        try:
+            assert region_dict[region].size > 0
+            region_dict[region] = np.vstack((region_dict[region],ave_data[i,:]))
+        except KeyError:
+            region_dict.setdefault(region, ave_data[i,:])
+
+    # average regions
+    lfp_dict = {region: np.average(arr, axis=0) for region, arr in region_dict.items()}
+
+    # concatenate into 1D array in given "order"
+
+    lfp_vector = np.array(())
+    for region in order:
+        lfp_vector = np.hstack((lfp_vector, lfp_dict[region]))
+
+    return lfp_vector
+
 
 def read_fa_map(fpath):
     """Return vectorized FA map.
@@ -24,9 +79,3 @@ def read_fa_map(fpath):
     img = nb.load(fpath)
     data = img.get_fdata()
     return np.reshape(data, data.size)
-
-
-#subjid='sub-ON97504'
-fpath='sub-ON97504_ses-01_task-haririhammer_ave.fif'
-ch_regions=read_chnames(fpath)
-print(ch_regions)
